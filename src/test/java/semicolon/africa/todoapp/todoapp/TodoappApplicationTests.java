7d4096c8fed02ce36c00136dd56377c165dae282
Ololade0 <adesuyiololade@gmail.com>
2022-11-29 20:59:46 +0100

    todo-app

---
+++ src/test/java/semicolon/africa/todoapp/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package semicolon.africa.todoapp.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
